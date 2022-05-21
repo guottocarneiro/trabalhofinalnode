b4ad2db1711d96e80afc943fc7ef47756292da5b
Renan <renancossenzo@hotmail.com>
2022-05-21 19:44:58 -0300

    Adicionando tratamento de exceção

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -1,4 +1,4 @@
-const express = require ('express');
+const express = require('express');
 const { fetchAllMovies } = require('../../services/imdbService') || {};
 let apiRouter = express.Router();
 const Usuario = require('../../dtos/usuario');
@@ -12,19 +12,25 @@ apiRouter.get('/filmes', async (req, res) => {
 });
 
 apiRouter.post('/usuarios', async (req, res) => {
-    const usuario = new Usuario(req.body.nome,req.body.login,req.body.senha);
-    const idNovoUsuario = await criarUsuario(usuario);
-    console.log(idNovoUsuario);
+    try {
+        const usuario = new Usuario(req.body.nome, req.body.login, req.body.senha);
+        const idNovoUsuario = await criarUsuario(usuario);
 
-    res.status(200).json(idNovoUsuario);
+        res.status(201).json(idNovoUsuario);
+    } catch (error) {
+        res.status(500).json({ message: `Erro ao cadastrar usuário - ${error.message}` })
+    }
 });
 
 apiRouter.post('/login', async (req, res) => {
-   
-    const resultadoLogin = await loginUsuario({login: req.body.login, senha: req.body.senha});
-    console.log(resultadoLogin);
+    try {
 
-    res.status(200).json(resultadoLogin);
+        const resultadoLogin = await loginUsuario({ login: req.body.login, senha: req.body.senha });
+
+        res.status(201).json(resultadoLogin);
+    } catch (error) {
+        res.status(500).json({ message: `Erro ao realizar login - ${error.message}` })
+    }
 });
 
 module.exports = apiRouter;
